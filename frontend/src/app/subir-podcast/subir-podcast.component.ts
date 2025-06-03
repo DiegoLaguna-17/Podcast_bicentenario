5c6846ac69b03ded13351ec75836b941d065de87
Adriana-Alvarez <adriana.alvarez.c@ucb.edu.bo>
2025-06-03 01:51:16 -0400

    subir episodio y podcast

---
+++ frontend/src/app/subir-podcast/subir-podcast.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subir-podcast',
+  imports: [],
+  templateUrl: './subir-podcast.component.html',
+  styleUrl: './subir-podcast.component.css'
+})
+export class SubirPodcastComponent {
+
+}
